bd0c3fd1948995bf9f84373b6a05c7e5ad4c1a3e
fer23270 <128810746+fer23270@users.noreply.github.com>
2024-02-02 12:07:43 +0100

    Update Practica3.java Segunda modificacion desde GitHub

--- src/Practica3.java
+++ src/Practica3.java
@@ -3,7 +3,9 @@ class Practica3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1a modificació Git");	
+		System.out.println("1a modificació Git");
+		//Segunda modificacion GitHub
+		System.out.println("2a modificació Git");
 	}
 
 }
